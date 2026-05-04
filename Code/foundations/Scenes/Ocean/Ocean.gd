4caef2ea701e1af99fab38561cff4aba33727acb
Kartikeya Chauhan <kc1.408596@gmail.com>
2026-05-05 01:47:34 +0530

    foundations completed

--- Code/foundations/Scenes/Ocean/Ocean.gd
+++ Code/foundations/Scenes/Ocean/Ocean.gd
@@ -1,8 +1,9 @@
 extends Node2D
 
 
-@onready var plane: Sprite2D = $Plane
+@onready var plane: Sprite2D = %Plane
 @onready var heli: Sprite2D = $Heli
+@onready var eating_sound: AudioStreamPlayer = $"Eating Sound"
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -10,14 +11,21 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	plane.move_local_x(100*delta)
-	heli.move_local_x(60*delta)
-	heli.rotate(-1.5*delta)
+	#var dir: Vector2 = plane.position.direction_to(heli.position)
+	plane.look_at(get_global_mouse_position())
+	plane.move_local_x(200)
+	#heli.position.y += 20.0 * delta
+	#heli.move_local_x(60*delta)
+	#heli.rotate(-1.5*delta)
 	
 	
 	if Input.is_action_pressed("ui_left"):
 		plane.rotate(-1.5 * delta)
 	if Input.is_action_pressed("ui_right"):
 		plane.rotate(1.5 * delta)
+	if Input.is_action_pressed("ui_accept"):
+		#eating_sound.play()
+		plane.global_position = Vector2(350,150)
+		
 		
 		
