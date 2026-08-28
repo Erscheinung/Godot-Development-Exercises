4e4fe577fd625f3099a7570892e8ebddbe72cb2a
Kartikeya Chauhan <kc1.408596@gmail.com>
2026-08-28 23:06:23 +0530

    dice catcher preload practice

--- Code/DiceCatcher/scenes/game/game.gd
+++ Code/DiceCatcher/scenes/game/game.gd
@@ -1,14 +1,16 @@
 extends Node2D
 
-@export var dice_scene: PackedScene
+#@export var dice_scene: PackedScene
+var DICE = preload("res://scenes/dice/dice.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:	
 	spawn_dice()
 	
 func spawn_dice() -> void:
-	var new_dice: Dice = dice_scene.instantiate()
-	add_child(new_dice)
+	#var new_dice: Dice = dice_scene.instantiate()
+	#add_child(new_dice)
+	pass
 
 func _on_spawn_timer_timeout() -> void:
 	spawn_dice()
